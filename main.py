864b4cac0b36f73ae443b6ebaa0cac999e2fd18c
Bruno Martins <brunomartinsdemaria@gmail.com>
2022-11-26 14:30:51 -0300

    Update main.py
    Add Butoons and entry in grid.

--- main.py
+++ main.py
@@ -16,12 +16,30 @@ canvas.create_image(100,100 ,image=logo_image) #cria dentro do canvas uma imagem
 canvas.grid(row=0, column=1)
 
 #Etiquetas/Labels:
-website_label = Label(text="Website") #é necessário colocar de mod "text=" para poder colocar a str.
+website_label = Label(text="Website:") #é necessário colocar de mod "text=" para poder colocar a str.
 website_label.grid(row=1 , column=0)
-email_label = Label(text="E-mail/Username")
+email_label = Label(text="E-mail/Username:")
 email_label.grid(row=2 , column=0)
-password_label = Label(text="Password")
+password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
+
+#ENTRADAS/Entrys:
+website_entry = Entry(width=35) #tamanho da janela do imput/entrada
+website_entry.grid(row=1, column=1)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+#Botoões/Buttons:
+generate_password = Button(text="GEnerate Password")
+generate_password.grid(row=3, column=2)
+add_button = Radiobutton(text="Add")
+add_button.grid(row=4, column=1)
+
+
+
+
 
 
 window.mainloop()
